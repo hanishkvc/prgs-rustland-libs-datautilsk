80e0f5391cc387bfa9ab960feffd0a8386bbef18
hanishkvc <hanishkvc@gmail.com>
2022-11-10 14:47:19 +0530

    Hex/Integer: Chop out the other part

--- src/hex.rs
+++ src/hex.rs
@@ -1,5 +1,5 @@
 //!
-//! Some data related utility functions
+//! Hex data related utility functions
 //!
 //! HanishKVC, 2022
 //!
@@ -48,46 +48,3 @@ pub fn hex_from_vu8(inv: &Vec<u8>) -> String {
     outs
 }
 
-
-///
-/// Allow conversion btw isize and u8 through a minimal wrapper around u8
-/// Additionally this allows conversion only if the isize value fits within u8 space
-/// else it will panic with a error message.
-/// This also helps make intvalue generic wrt the types I want (ie isize and u8 immidiately)
-///
-
-#[derive(Debug)]
-pub struct U8X(pub u8);
-
-impl Into<u8> for U8X {
-    fn into(self) -> u8 {
-        let U8X(u8val) = self;
-        return u8val;
-    }
-}
-
-impl From<isize> for U8X {
-    fn from(ival: isize) -> Self {
-        if (ival < 0) || (ival > u8::MAX.into()) {
-            panic!("ERRR:DU:U8XFromISize:isize{} beyond u8 range", ival);
-        }
-        let uval = ival as usize;
-        return U8X(uval as u8);
-    }
-}
-
-///
-/// Convert given string value to a isize, by treating it has a decimal
-/// or hexdecimal (if starts with 0x) string value.
-///
-/// Inturn try convert the isize to specified type.
-pub fn intvalue<T: std::convert::From<isize>>(sval: &str) -> Result<T, ParseIntError> {
-    let sval = sval.trim();
-    let ival;
-    if sval.starts_with("0x") {
-        ival = isize::from_str_radix(&sval[2..], 16)?;
-    } else {
-        ival = isize::from_str_radix(sval, 10)?;
-    }
-    return Ok(T::try_from(ival).unwrap());
-}
--- src/integer.rs
+++ src/integer.rs
@@ -1,52 +1,11 @@
 //!
-//! Some data related utility functions
+//! Some Integer data related utility functions
 //!
 //! HanishKVC, 2022
 //!
 
 use core::convert::From;
 use std::num::ParseIntError;
-
-
-///
-/// Routines to help convert between hex string and Vec<u8>
-///
-
-
-///
-/// Convert hex string to Vec<u8>
-///
-pub fn vu8_from_hex(ins: &str) -> Result<Vec<u8>, String> {
-    if ins.len() % 2 != 0 {
-        return Err("ERRR:DU:Vu8FromHex:Hex string length not even, something wrong???".to_string());
-    }
-    let mut vu8 = Vec::new();
-    for i in (0..ins.len()-1).step_by(2) {
-        let cu8 = u8::from_str_radix(&ins[i..i+2], 16);
-        if cu8.is_err() {
-            return Err(format!("ERRR:DU:VU8FromHex:{}>>{}<<:{}", ins, &ins[i..i+2], cu8.unwrap_err()));
-        }
-        vu8.push(cu8.unwrap());
-    }
-    Ok(vu8)
-}
-
-///
-/// Convert Vec<u8> to hex string
-///
-pub fn hex_from_vu8(inv: &Vec<u8>) -> String {
-    let hex = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-    let mut outs = String::new();
-    for i in 0..inv.len() {
-        let cu8 = inv[i];
-        let bhigh = (cu8 & 0xF0) >> 4;
-        let blow = cu8 & 0x0F;
-        //log_d(&format!("DBUG:DU:HexFromVU8:{}+{}+{}", outs, bhigh, blow));
-        outs.push_str(hex[bhigh as usize]);
-        outs.push_str(hex[blow as usize]);
-    }
-    outs
-}
 
 
 ///
